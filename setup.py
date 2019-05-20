5a0db7cad3fd652cdea4948923ef89455e79c8ca
Amadeus Gebauer <gebauer@lnm.mw.tum.de>
2019-05-20 17:02:19 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup
 
 setup(
     name='lnmmeshio',
-    version='1.0',
+    version='1.0.2',
     packages=['lnmmeshio',],
     license='unlicensed. All rights reserved.',
     long_description=open('README.md').read(),
+    install_requires=[
+        'progress>=1.5',
+        'numpy>=1.16.3',
+        'python-utils>=2.3.0'
+    ]
 )
