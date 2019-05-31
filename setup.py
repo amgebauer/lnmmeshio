bf4ba5ce0c62512d835f53c8dadded9ffcd51e11
Amadeus Gebauer <gebauer@lnm.mw.tum.de>
2019-05-31 14:45:05 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lnmmeshio',
-    version='2.1.0',
+    version='2.1.1',
     packages=['lnmmeshio',],
     license='unlicensed. All rights reserved.',
     long_description=open('README.md').read(),
