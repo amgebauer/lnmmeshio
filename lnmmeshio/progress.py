56830382df88ec3da12fb94c2d25c9f32c5bf718
Amadeus Gebauer <gebauer@lnm.mw.tum.de>
2019-08-16 11:03:05 +0200

    update progress

--- lnmmeshio/progress.py
+++ lnmmeshio/progress.py
@@ -1,4 +1,5 @@
 from progress import bar, spinner
+import time
 
 def progress(iterator, out = True, label=None, btype='bar'):
 
@@ -16,8 +17,30 @@ def progress(iterator, out = True, label=None, btype='bar'):
             label = 'progress'
         
         if max_val < 0:
-            return spinner.Spinner(label).iter(iterator)
+            return ProgressIterator(spinner.Spinner(label).iter(iterator), label)
         else:
-            return bar.Bar(label).iter(iterator)
+            return ProgressIterator(bar.Bar(label).iter(iterator), label)
     else:
         return iterator
+
+def clearln():
+    print('\033[F\r\x1b[K', end='')
+
+class ProgressIterator:
+
+    def __init__(self, iterator, label):
+        self.iterator = iterator
+        self.starttime = time.time()
+        self.label = label
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return self.iterator.__next__()
+        except StopIteration:
+            # print total time
+            clearln()
+            print('{0} ... {1:.2f}s'.format(self.label, time.time()-self.starttime))
+            raise StopIteration
